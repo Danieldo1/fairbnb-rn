e8b14837cc174ac94b727f4fbf0f5786ea4a37f6
Dan <daniel.speranskiy@live.com>
2023-11-14 12:32:12 +0400

    Explore page taking shape added listings and some of their values

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,21 +1,84 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, FlatList, ListRenderItem, TouchableOpacity, StyleSheet, Image } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import { defaultStyles } from '@/constants/Styles'
+import { Link } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import Animated, { FadeOutLeft, FadeInRight } from 'react-native-reanimated'
 
 interface Props {
     listings: any[]
     category: string
 }
 
-const Listings = ({listings, category}: Props) => {
+const Listings = ({listings:items, category}: Props) => {
+  const [loading, setLoading] = useState(false)
+  const listRef = useRef<FlatList>(null)
+  const renderItem:ListRenderItem<any> = ({item}) => {
+    if (!item.medium_url) {
+      return null; 
+    }
+  
+    return (
+      <Link href={`/listing/${item.id}`} asChild>
+        {/* Photo */}
+        <TouchableOpacity>
 
+          <Animated.View style={styles.listing} entering={FadeInRight} exiting={FadeOutLeft}>
+            <Image source={{uri: item.medium_url}} style={styles.img} />
+            <TouchableOpacity style={{position: 'absolute', top: 30, right: 30}}>
+              <Ionicons name='heart-outline' size={24} color={'#000'} />
+            </TouchableOpacity>
+            {/* Name star */}
+            <View style={{marginTop: 10, flexDirection: 'row', justifyContent: 'space-between'}}>
+              <Text style={{fontFamily: 'mon-sb', fontSize: 16}}>{item.name.length > 20 ? `${item.name.slice(0, 30)}...` : item.name}</Text>
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Ionicons name='star' size={14} color={'#000'}  />
+                <Text style={{fontFamily: 'mon-sb', marginLeft: 5, gap: 5}}>{item.review_scores_rating/20}</Text>
+              </View>
+            </View>
+        {/* Room type */}
+            <Text style={{fontFamily: 'mon', color: '#636262'}}>{item.room_type}</Text>
+        {/* Price */}
+            <View style={{flexDirection: 'row', alignItems: 'center', gap: 10}}>
+              <Text style={{fontFamily: 'mon-sb'}}>$ {item.price}</Text>
+              <Text style={{fontFamily: 'mon', color: '#636262'}}>night</Text>
+            </View>
+          
+          </Animated.View>
+        </TouchableOpacity>
+      </Link>
+    );
+  }
+  
     useEffect(() => {
-        console.log("Reload",listings.length)
-    },[category])
-  return (
-    <View>
-      <Text>Listings</Text>
+      setLoading(true)
+
+        setTimeout(() => {
+            setLoading(false)
+          }, 100)
+        },[category])
+        return (
+          <View style={defaultStyles.box}>
+      <FlatList 
+      renderItem={renderItem}
+      data={loading ? [] : items}
+      ref={listRef}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  listing: {
+    padding: 20,
+    gap: 10,
+    marginVertical: 16,
+  },
+  img: {
+    width: '100%',
+    height: 300,
+    borderRadius: 10,
+  },
+})
 
 export default Listings
