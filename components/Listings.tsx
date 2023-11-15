728f29465c16cdc9953a276ad447c6cd9ec81b9d
Dan <daniel.speranskiy@live.com>
2023-11-15 10:22:00 +0400

    Added a few styles

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -5,6 +5,7 @@ import { Link } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
 import Animated, { FadeOutLeft, FadeInRight } from 'react-native-reanimated'
 import { BottomSheetFlatList, BottomSheetFlatListMethods } from '@gorhom/bottom-sheet'
+import Colors from '@/constants/Colors'
 
 interface Props {
     listings: any[]
@@ -20,11 +21,18 @@ const Listings = ({listings:items, category,refresh}: Props) => {
           listRef.current?.scrollToOffset({ animated: true, offset: 0 })
       }
   },[refresh])
+  const [isTapped, setIsTapped] = useState<{[itemId: string]: boolean}>({}); 
+  const handleTap = (itemId: string) => {
+    setIsTapped((prevIsTapped) => ({
+      ...prevIsTapped,
+      [itemId]: !prevIsTapped[itemId]
+    }));
+  };
   const renderItem:ListRenderItem<any> = ({item}) => {
     if (!item.medium_url) {
       return null; 
     }
-
+   
   
     return (
       <Link href={`/listing/${item.id}`} asChild>
@@ -33,9 +41,13 @@ const Listings = ({listings:items, category,refresh}: Props) => {
 
           <Animated.View style={styles.listing} entering={FadeInRight} exiting={FadeOutLeft}>
             <Image source={{uri: item.medium_url}} style={styles.img} />
-            <TouchableOpacity style={{position: 'absolute', top: 30, right: 30}}>
-              <Ionicons name='heart-outline' size={24} color={'#000'} />
-            </TouchableOpacity>
+            <TouchableOpacity style={{ position: 'absolute', top: 30, right: 30,backgroundColor: '#fff', borderRadius: 50, padding: 3,borderColor: '#000', borderWidth: 0.25 }} onPress={() => handleTap(item.id)}>
+            <Ionicons
+              name={isTapped[item.id] ? 'heart' : 'heart-outline'}
+              size={24}
+              color={isTapped[item.id] ? Colors.primary : '#000'}
+            />
+          </TouchableOpacity>
             {/* Name star */}
             <View style={{marginTop: 10, flexDirection: 'row', justifyContent: 'space-between'}}>
               <Text style={{fontFamily: 'mon-sb', fontSize: 16}}>{item.name.length > 20 ? `${item.name.slice(0, 30)}...` : item.name}</Text>
